599e4199fb1d2facdb0b803ba6a91b4ca478fb23
Viktor Shishkalov <viktorshish@gmail.com>
2023-09-16 23:16:51 +0300

    Добавил недостающий аргумент в функцию получения фото и Оформил README

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -5,13 +5,13 @@ import requests
 from get_image import get_image
 
 
-def fetch_spacex_last_launch(spacex_url):
+def fetch_spacex_last_launch(spacex_url, image_name):
     response = requests.get(spacex_url)
     response.raise_for_status()
     image_url = response.json()['links']['flickr']['original']
 
     for image_number, image_url in enumerate(image_url):
-        get_image(image_url, image_number)
+        get_image(image_url, image_number, image_name)
 
     return response.json()
 
@@ -21,12 +21,13 @@ def main():
         description='Скачивание фотографий с сайта SpaceX'
     )
     parser.add_argument('-id', '--launch_id', help='ID запуска')
+    parser.add_argument('name', help='Имя фотографии')
     args = parser.parse_args()
 
     spacex_url = f'https://api.spacexdata.com/v5/launches/{args.launch_id}'
 
     try:
-        fetch_spacex_last_launch(spacex_url)
+        fetch_spacex_last_launch(spacex_url, args.name)
     except requests.exceptions.HTTPError:
         exit('Фотографий с последнего запуска нет')
 
